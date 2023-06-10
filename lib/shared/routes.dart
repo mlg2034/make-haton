bb9918eee374f81e380ddbd61351c94a9824a9d0
“marvelaman” <30561254+marvelaman@users.noreply.github.com>
2023-06-10 19:53:23 +0600

    routes

---
+++ lib/shared/routes.dart
@@ -0,0 +1,9 @@
+abstract class Routes {
+  static const splashPage = '/splashPage';
+  static const authPage = '/authPage';
+  static const homePage = '/homePage';
+  static const settingsPage = '/settingsPage';
+  static const learnPage = '/learnPage';
+  static const practicePage = '/practicePage';
+  static const characterPage = '/characterPage';
+}
