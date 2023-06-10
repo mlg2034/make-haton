1cb2718b07790cdf713bf095bbd113cad7d21f6f
“marvelaman” <30561254+marvelaman@users.noreply.github.com>
2023-06-11 04:28:55 +0600

    ref

--- lib/src/ui/presentation/pages/home_page/home_page.dart
+++ lib/src/ui/presentation/pages/home_page/home_page.dart
@@ -55,96 +55,98 @@ class HomePage extends StatelessWidget {
         //   ),
         // ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Divider(),
-          const Spacer(),
-          SvgPicture.asset(
-            UiKitAssets.images.imNotebookFrontGradient.keyName,
-          ),
-          // LottieWidget(lottieKeyName: UiKitAssets.lottie.robot.keyName),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 48.0, vertical: 24),
-            child: Row(
-              children: [
-                Expanded(
-                  child: AppOutlinedButton.rect(
-                    aspectRatio: 158 / 64,
-                    child: Center(
-                      child: SvgPicture.asset(
-                        UiKitAssets.icons.icCollections.keyName,
-                        width: 25,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Divider(),
+            const Spacer(),
+            SvgPicture.asset(
+              UiKitAssets.images.imNotebookFrontGradient.keyName,
+            ),
+            // LottieWidget(lottieKeyName: UiKitAssets.lottie.robot.keyName),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 48.0, vertical: 24),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: AppOutlinedButton.rect(
+                      aspectRatio: 158 / 64,
+                      child: Center(
+                        child: SvgPicture.asset(
+                          UiKitAssets.icons.icCollections.keyName,
+                          width: 25,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: AppOutlinedButton.rect(
-                    onPressed: () => Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => const SettingsPage())),
-                    aspectRatio: 158 / 64,
-                    child: Center(
-                      child: SvgPicture.asset(
-                        UiKitAssets.icons.icSettings.keyName,
-                        width: 25,
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: AppOutlinedButton.rect(
+                      onPressed: () => Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => const SettingsPage())),
+                      aspectRatio: 158 / 64,
+                      child: Center(
+                        child: SvgPicture.asset(
+                          UiKitAssets.icons.icSettings.keyName,
+                          width: 25,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 48.0),
-            child: ProgressBar(),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
-            l.wordsLearnedTitle(15),
-            style: button,
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 48.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: AppOutlinedButton.rect(
-                    innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
-                    aspectRatio: _kAspectRatio,
-                    child: SvgPicture.asset(
-                      UiKitAssets.icons.trencher.keyName,
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 48.0),
+              child: ProgressBar(),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              l.wordsLearnedTitle(15),
+              style: button,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 48.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: AppOutlinedButton.rect(
+                      innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      aspectRatio: _kAspectRatio,
+                      child: SvgPicture.asset(
+                        UiKitAssets.icons.trencher.keyName,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: AppOutlinedButton.rect(
-                    innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
-                    aspectRatio: _kAspectRatio,
-                    child: SvgPicture.asset(
-                      UiKitAssets.icons.books.keyName,
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: AppOutlinedButton.rect(
+                      innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      aspectRatio: _kAspectRatio,
+                      child: SvgPicture.asset(
+                        UiKitAssets.icons.books.keyName,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-        ],
+            const SizedBox(
+              height: 32,
+            ),
+          ],
+        ),
       ),
     );
   }
