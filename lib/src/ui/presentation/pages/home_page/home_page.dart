64867f574dd2f0f32b813856a98ace18abe55ac0
“marvelaman” <30561254+marvelaman@users.noreply.github.com>
2023-06-11 03:56:56 +0600

    ref

--- lib/src/ui/presentation/pages/home_page/home_page.dart
+++ lib/src/ui/presentation/pages/home_page/home_page.dart
@@ -17,8 +17,9 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 120,
         title: Padding(
-          padding: const EdgeInsets.all(48.0),
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
           child: Row(
             children: [
               Column(
@@ -30,13 +31,16 @@ class HomePage extends StatelessWidget {
               const Spacer(),
               FakeButton(
                 child: Center(
-                  child: Row(
-                    children: [
-                      SvgPicture.asset(
-                        UiKitAssets.icons.coin.keyName,
-                      ),
-                      const Text('200'),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          UiKitAssets.icons.coin.keyName,
+                        ),
+                        const Text('200'),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -51,98 +55,96 @@ class HomePage extends StatelessWidget {
         //   ),
         // ),
       ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Divider(),
-            const Spacer(),
-            SvgPicture.asset(
-              UiKitAssets.images.imNotebookFrontGradient.keyName,
-            ),
-            // LottieWidget(lottieKeyName: UiKitAssets.lottie.robot.keyName),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 48.0, vertical: 24),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: AppOutlinedButton.rect(
-                      aspectRatio: 158 / 64,
-                      child: Center(
-                        child: SvgPicture.asset(
-                          UiKitAssets.icons.icCollections.keyName,
-                          width: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: AppOutlinedButton.rect(
-                      onPressed: () => Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) => const SettingsPage())),
-                      aspectRatio: 158 / 64,
-                      child: Center(
-                        child: SvgPicture.asset(
-                          UiKitAssets.icons.icSettings.keyName,
-                          width: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 48.0),
-              child: ProgressBar(),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              l.wordsLearnedTitle(15),
-              style: button,
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 48.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: AppOutlinedButton.rect(
-                      innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
-                      aspectRatio: _kAspectRatio,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Divider(),
+          const Spacer(),
+          SvgPicture.asset(
+            UiKitAssets.images.imNotebookFrontGradient.keyName,
+          ),
+          // LottieWidget(lottieKeyName: UiKitAssets.lottie.robot.keyName),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0, vertical: 24),
+            child: Row(
+              children: [
+                Expanded(
+                  child: AppOutlinedButton.rect(
+                    aspectRatio: 158 / 64,
+                    child: Center(
                       child: SvgPicture.asset(
-                        UiKitAssets.icons.trencher.keyName,
+                        UiKitAssets.icons.icCollections.keyName,
+                        width: 25,
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Expanded(
-                    child: AppOutlinedButton.rect(
-                      innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
-                      aspectRatio: _kAspectRatio,
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: AppOutlinedButton.rect(
+                    onPressed: () => Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => const SettingsPage())),
+                    aspectRatio: 158 / 64,
+                    child: Center(
                       child: SvgPicture.asset(
-                        UiKitAssets.icons.books.keyName,
+                        UiKitAssets.icons.icSettings.keyName,
+                        width: 25,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 32,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 48.0),
+            child: ProgressBar(),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            l.wordsLearnedTitle(15),
+            style: button,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: AppOutlinedButton.rect(
+                    innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    aspectRatio: _kAspectRatio,
+                    child: SvgPicture.asset(
+                      UiKitAssets.icons.trencher.keyName,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: AppOutlinedButton.rect(
+                    innerPadding: const EdgeInsets.symmetric(horizontal: 32.0),
+                    aspectRatio: _kAspectRatio,
+                    child: SvgPicture.asset(
+                      UiKitAssets.icons.books.keyName,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+        ],
       ),
     );
   }
